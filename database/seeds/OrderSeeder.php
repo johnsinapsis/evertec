98c89374947932c6c17c5ee51a4a7a2957681b87
john jairo gonzalez <johnsinapsis@gmail.com>
2021-01-21 20:33:03 -0500

    Ajuste en el OrderSeeder. se Agregan datos

--- database/seeds/OrderSeeder.php
+++ database/seeds/OrderSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Order;
 use Illuminate\Database\Seeder;
 
 class OrderSeeder extends Seeder
@@ -11,6 +12,21 @@ class OrderSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Order::truncate();
+        Order::create([
+            'user_id' => 2,
+            'status' => 'CREATED',
+            'url' => 'https://test.placetopay.com/redirection/session/446835/741a2fa8cb3e01e689782603e71b218e'
+        ]);
+        Order::create([
+            'user_id' => 2,
+            'status' => 'PAYED',
+            'url' => null
+        ]);
+        Order::create([
+            'user_id' => 2,
+            'status' => 'REJECTED',
+            'url' => 'https://test.placetopay.com/redirection/session/446835/741a2fa8cb3e01e689782603e71b218e'
+        ]);
     }
 }
